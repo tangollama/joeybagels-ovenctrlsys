4ef223a8e7f314fb3db6960f6793b380446c20db
Joel Worrall <jworrall@newrelic.com>
2018-08-25 16:48:10 -0400

    initial check in

---
+++ src/ovenctrlsys.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"os"
+	"encoding/json"
+
+	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
+	"github.com/newrelic/infra-integrations-sdk/data/event"
+	"github.com/newrelic/infra-integrations-sdk/data/metric"
+	"github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/newrelic/infra-integrations-sdk/log"
+	"testdata"
+)
+
+type argumentList struct {
+	sdkArgs.DefaultArgumentList
+}
+
+const (
+	integrationName    = "com.joeybagels.ovenctrlsys"
+	integrationVersion = "0.1.0"
+)
+
+var (
+	args argumentList
+)
+
+func main() {
+	var data ovenctlsys.Status
+	byt := []byte(ovenctlsys.Simulate())
+	if err := json.Unmarshal(byt, &data); err != nil {
+		//fmt.Println("There was an error:", err)
+		log.Error(err.Error())
+		os.Exit(1)
+    }
+    //fmt.Println(dat)
+
+	// Create Integration
+	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// Create Entity, entities name must be unique
+	e1, err := i.Entity("overctrlsys", "custom")
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	// Add Event
+	if data.Restart == true {
+		err = e1.AddEvent(event.NewNotification("restart"))
+		if err != nil {
+			log.Error(err.Error())
+		}
+	}
+
+	// Add Inventory item
+	if data.Version != "" {
+		err = e1.SetInventoryItem("ctrlsys", "version", data.Version)
+		if err != nil {
+			log.Error(err.Error())
+		}
+	}
+
+	// Add Metric
+	if data.Temp > -1 {
+		m1 := e1.NewMetricSet("ctrlsys")
+		err = m1.SetMetric("temp", data.Temp, metric.GAUGE)
+		if err != nil {
+			log.Error(err.Error())
+		}
+	}
+
+	if err = i.Publish(); err != nil {
+		log.Error(err.Error())
+	}
+}
---
+++ src/testdata/testdata.go
@@ -0,0 +1,39 @@
+package ovenctlsys
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+/*
+Simulating a JSON object in the following form
+
+{
+	"temp": 450,		//temperature of the oven
+	"version": "1.0.4",	//software version of the control software
+	"restart": false 	//boolean of restart Event
+}
+*/
+
+type Status struct {
+	Temp int `json:"temp"`
+	Version string `json:"version"`
+	Restart bool `json:"restart"`
+}
+
+func Simulate() (output string) {
+
+	temps := []int {450,500,400,450,375,425,375,400,450,525,650,325,350,375,400}
+
+	rand.Seed(time.Now().UnixNano())
+	index := rand.Intn(len(temps)-1)
+	restart := index%8 == 0	//mark restart on a random index of 8
+	output = fmt.Sprintf(`{ "temp": %d, "version": "%s", "restart": %t }`, temps[index], "1.0.1", restart)
+	return output
+}
+
+func main() {
+	v := Simulate()
+	fmt.Println(v)
+}
+
